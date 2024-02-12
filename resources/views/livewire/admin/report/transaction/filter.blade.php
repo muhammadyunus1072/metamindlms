005a113bf7bd1c50b7455dc2ed5086ef6385aa5a
Muhammad Yunus <muhammadyunus1072@gmail.com>
2024-02-12 17:43:39 +0700

    fix

--- resources/views/livewire/admin/report/transaction/filter.blade.php
+++ resources/views/livewire/admin/report/transaction/filter.blade.php
@@ -59,7 +59,7 @@
         </div>
     </div>
     {{-- Card Total --}}
-    <div class="row col-12 my-4 d-flex justify-content-evenly align-items-stretch">
+    <div class="row col-12 my-4 d-flex justify-content-around align-items-stretch">
         <div class="col-md-5 mb-2">
             <div class="card">
                 <div class="card-header text-center" style="color:white;">
